4979cb4e557607af5d66922dfcad7a2d6fc068f4
Jens Jansson <peppe@vaadin.com>
2018-04-26 21:45:50 +0200

    Replaced template with basic V8-style example

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -2,7 +2,9 @@ package com.vaadin.starter.skeleton;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -13,12 +15,13 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        ExampleTemplate template = new ExampleTemplate();
+        final TextField name = new TextField();
+        name.setLabel("Type your name here:");
 
         Button button = new Button("Click me",
-                event -> template.setValue("Clicked!"));
+                event -> add(new Label("Thanks " + name.getValue() + ", it works!")));
 
-        add(button, template);
+        add(name, button);
         setClassName("main-layout");
     }
 }
